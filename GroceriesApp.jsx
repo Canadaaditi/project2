60a3caf56293ee7c4c3dd0ee547b3c1aaf465da2
Canadaaditi <145692532+Canadaaditi@users.noreply.github.com>
2023-12-04 19:27:18 -0500

    Add files via upload

---
+++ GroceriesApp.jsx
@@ -0,0 +1,144 @@
+import InventoryCard from "./InventoryCard";
+import CartList from "./CartList";
+//import { products } from "./data/products";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import InventoryForm from "./InventoryForm";
+
+export default function GroceriesApp() {
+  const emptyProduct ={
+    id: "",
+    productName: "",
+    barnd: "",
+    quantity: "",
+    image: "",
+    price: "",
+  };
+  const [cartList, setCartList] = useState([]);
+  const[products, setProducts] = useState([]);
+  const[formData, setFormData] = useState(emptyProduct);
+  const [responseData, setResponseData] = useState("")
+  const [toggleEdit, setToggleEdit] = useState(false);
+
+  //useEffect
+  useEffect(() => {
+ handleGetProducts();
+  },[responseData]);
+
+//HANDLERS
+//Get Products
+  const handleGetProducts = async() => {
+  await axios.get("http://localhost:3000/products").then((response) => 
+  setProducts(response.data)
+  );
+
+  };
+//Post Product
+const handlePostProduct = async (product) => {
+  const postProduct = {
+    id: product.id,
+    productName: product.productName,
+    brand: product.brand,
+    quantity: product.quantity,
+    image: product.image,
+    price: product.price
+  };
+  await axios
+  .post("http://localhost:300/addProduct", postProduct)
+  .then(response => setResponseData(<p>{response.data}</p>));
+};
+
+const handleOnSubmit = (evt) => {
+evt.preventDefault();
+toggleEdit ? handleProductEdit(formData) : handlePostProduct(formData);
+setFormData(emptyProduct);
+};
+
+const handleProductDelete = async (product) => {
+  const id = product._id;
+  axios
+  .delete(`http://localhost:3000/products/${id}`)
+  .then((response) => setResponseData(<p>{response.data}</p>));
+};
+
+const handleProductEdit = async (product) => {
+  const id = product._id;
+  const editData= {
+    id: product.id,
+    productName: product.productName,
+    brand: product.brand,
+    quantity: product.quantity,
+    image: product.image,
+    price: product.price,
+  };
+  await axios.patch(`http://localhost:3000/product/${id}`, editData)
+  .then((response) => setResponseData(<p>{response.data}</p>))
+  .then(setToggleEdit(false));
+
+};
+
+
+const handleToggleEdit = (product) =>{
+  setFormData(product);
+  setToggleEdit(true);
+};
+
+  //Adding To cart
+  const handleAddToCart = (item) => {
+    setCartList((prevList) => {
+      console.log(cartList);
+      return [...prevList, { ...item, id: crypto.randomUUID() }];
+    });
+  };
+
+  //Onchange Handler
+ const handleOnChange = (evt) =>{
+const fieldName = evt.target.name
+ const fieldValue = evt.target.value
+ setFormData((prevData) => {
+  return {...prevData, 
+    id: crypto.randomUUID(),
+    [fieldName]: fieldValue,
+  };
+ });
+};
+
+//Emptying Cart
+  const handleEmptyCart = () => {
+    setCartList([]);
+  };
+
+
+//Removing Item from Cart
+  const handleRemoveItem = (id) => {
+    setCartList((prevList) => {
+      return prevList.filter((i) => i.id !== id);
+    });
+  };
+
+  return (
+    <>  
+      <h1>Groceries App</h1>
+      <InventoryForm 
+      formData={formData}
+      handleOnChange={handleOnChange}
+      handleOnSubmit={handleOnSubmit}
+      toggleEdit={toggleEdit}
+      />
+      {responseData}
+      <div className="GroceriesApp-Container">
+        <InventoryCard 
+        list={products}
+        onClick={handleAddToCart}
+        handleProductDelete={handleProductDelete}
+        handleToggleEdit={handleToggleEdit} 
+        />
+        <CartList
+          cartList={cartList}
+          onClickEmpty={handleEmptyCart}
+          onClickRemove={handleRemoveItem}
+        />
+      </div>
+    </>
+  );
+}
